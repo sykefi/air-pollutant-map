fd8ed09e3586096c84d9eeb22a6ab1181f0947fb
Joose Helle <joose.helle@gmail.com>
2020-07-30 11:17:08 +0300

    Add dynamic legend for grid data layer

--- src/utils/PollutantStyles.tsx
+++ src/utils/PollutantStyles.tsx
@@ -1,4 +1,5 @@
 import { FeatureLike } from "ol/Feature";
+import { Pollutant } from "./../types";
 
 const pollutantBreakPointValues: { [key: string]: number[] } = {
   s16: [0.01, 0.03, 0.07, 5, 2576],
@@ -21,12 +22,50 @@ const getFeatureColor = (
   return "grey"; //return default color "grey" if value is outside the breakpoint ranges
 };
 
-export const getColorFunction = (pollutantIdentifier: string): Function | undefined => {
+export const getColorFunction = (pollutant: Pollutant): Function | undefined => {
   // e.g. getColorFunction("s16")
-  const breakPoints = pollutantBreakPointValues[pollutantIdentifier];
+  const breakPoints = pollutantBreakPointValues[pollutant.dbCol];
   if (!breakPoints) {
-    console.log("could not find color breakpoints for pollutant", pollutantIdentifier);
+    console.log(
+      "Could not find style function for pollutant",
+      pollutant.parlocRyhmaSelite,
+      pollutant.dbCol
+    );
     return undefined;
   }
-  return (feature: FeatureLike) => getFeatureColor(breakPoints, pollutantIdentifier, feature);
+  return (feature: FeatureLike) => getFeatureColor(breakPoints, pollutant.dbCol, feature);
+};
+
+interface ValueRange {
+  min: number;
+  max: number;
+  color: string;
+}
+
+export interface PollutantLegend {
+  classNames: number[];
+  [key: number]: ValueRange;
+  unit: string;
+}
+
+export const getPollutantLegendObject = (
+  pollutant: Pollutant
+): PollutantLegend | undefined => {
+  const breakPoints = pollutantBreakPointValues[pollutant.dbCol];
+  if (!breakPoints) {
+    console.log("Failed to load legend for pollutant", pollutant.parlocRyhmaSelite);
+    return undefined;
+  }
+  const pollutantLegend: PollutantLegend = breakPoints.reduce(
+    (legend, breakPoint, index) => {
+      const prevBreakPoint = index > 0 ? breakPoints[index - 1] : 0;
+      legend[index + 1] = { min: prevBreakPoint, max: breakPoint, color: colors[index] };
+      return legend;
+    },
+    {
+      classNames: [...Array(breakPoints.length).keys()].map((name) => name + 1),
+      unit: pollutant.yksikko
+    }
+  );
+  return pollutantLegend;
 };
