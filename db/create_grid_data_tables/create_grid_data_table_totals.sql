963fe504c526655ff4f29cc4ac9b0a43bf6e81bc
Joose Helle <joose.helle@gmail.com>
2020-08-10 10:30:26 +0300

    Create create_grid_data_table_totals.sql

---
+++ db/create_grid_data_tables/create_grid_data_table_totals.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS gd_totals;
+
+CREATE TABLE IF NOT EXISTS gd_totals AS (
+    SELECT GD.*, G.geom_web as geom
+    FROM 
+        (
+        SELECT vuosi,rivi, sum(s16) as s16, sum(s15) as s15, sum(s22) as s22, sum(s13) as s13, sum(s28) as s28, sum(s29) as s29, sum(s27) as s27, sum(s43) as s43, sum(s5) as s5, sum(s18) as s18, sum(s3) as s3, sum(s12) as s12, sum(s1) as s1, sum(s7) as s7, sum(s8) as s8, sum(s14) as s14, sum(s19) as s19, sum(s17) as s17, sum(s38) as s38, sum(s40) as s40 
+        FROM grid_data AS GD
+        GROUP BY vuosi,rivi
+        ) AS GD
+    INNER JOIN emep_grid_01 AS G ON G.ruutu_id = GD.rivi
+);
