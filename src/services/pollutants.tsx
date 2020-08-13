5321b764638e77c218d9cd47b3e304fc15343027
Joose Helle <joose.helle@gmail.com>
2020-08-13 15:48:05 +0300

    Fetch & show municipality data on the map

--- src/services/pollutants.tsx
+++ src/services/pollutants.tsx
@@ -4,6 +4,7 @@ import * as cache from "./cache";
 const gsUri = process.env.VUE_APP_GEOSERVER_URI;
 const gridDataTable = "p_gd_test";
 const gridDataTotalsTable = "p_gd_totals";
+const muniDataTable = "p_muni_data";
 const outputFormat = "&outputFormat=application/json";
 
 const getWfsGridDataUri = (year: number, gnfr: Gnfr, pollutant: Pollutant): string => {
@@ -18,7 +19,7 @@ const getWfsTotalGridDataUri = (year: number, pollutant: Pollutant) => {
     ${outputFormat}&viewparams=year:${year}`.replace(/ /g, "");
 };
 
-const getCacheKey = (year: number, gnfr: Gnfr, pollutant: Pollutant) => {
+const getGridDataCacheKey = (year: number, gnfr: Gnfr, pollutant: Pollutant) => {
   return `pollutant_map_grid_data_${year}_${gnfr}_${pollutant.dbCol}`;
 };
 
@@ -27,7 +28,7 @@ export const fetchFeatures = async (year: number, gnfr: Gnfr, pollutant: Polluta
     gnfr === Gnfr.COMBINED
       ? getWfsTotalGridDataUri(year, pollutant)
       : getWfsGridDataUri(year, gnfr, pollutant);
-  const cacheKey = getCacheKey(year, gnfr, pollutant);
+  const cacheKey = getGridDataCacheKey(year, gnfr, pollutant);
   const cached = cache.getFromCache(cacheKey);
   if (cached) {
     console.log("Fetched grid data from cache");
@@ -37,5 +38,30 @@ export const fetchFeatures = async (year: number, gnfr: Gnfr, pollutant: Polluta
   const fc = await response.json();
   cache.setToCache(cacheKey, fc);
   console.log("Fetched grid data from WFS");
+  return fc;
+};
+
+const getMuniDataCacheKey = (year: number, pollutant: Pollutant) => {
+  return `pollutant_map_muni_data_${year}_${pollutant.dbCol}`;
+};
+
+const getWfsMuniDataUri = (year: number, pollutant: Pollutant) => {
+  return `${gsUri}ows?service=WFS&version=1.0.0
+    &request=GetFeature&typeName=paastotkartalla:${muniDataTable}&propertyName=geom,nimi,area,${pollutant.dbCol}
+    ${outputFormat}&viewparams=year:${year}`.replace(/ /g, "");
+};
+
+export const fetchMuniFeatures = async (year: number, pollutant: Pollutant) => {
+  const uri = getWfsMuniDataUri(year, pollutant);
+  const cacheKey = getMuniDataCacheKey(year, pollutant);
+  const cached = cache.getFromCache(cacheKey);
+  if (cached) {
+    console.log("Fetched grid data from cache");
+    return cached;
+  }
+  const response = await fetch(encodeURI(uri));
+  const fc = await response.json();
+  cache.setToCache(cacheKey, fc);
+  console.log("Fetched muni data from WFS");
   return fc;
 };
