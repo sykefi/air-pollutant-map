a30f38d20b750157c886cf0743bf6dd701ad1cb3
Joose Helle <joose.helle@gmail.com>
2020-09-07 10:55:08 +0300

    Add fields for GNFR descriptions

--- db/create_metadata_tables/create_gnfr_meta_table.sql
+++ db/create_metadata_tables/create_gnfr_meta_table.sql
@@ -6,6 +6,9 @@ CREATE TABLE public.gnfr_meta
     nimi VARCHAR(30) NOT NULL,
     namn VARCHAR(30) NOT NULL,
     name VARCHAR(30) NOT NULL,
+    desc_fi VARCHAR(800),
+    desc_sv VARCHAR(800),
+    desc_en VARCHAR(800),
     use_dev BOOLEAN NOT NULL DEFAULT FALSE,
     use_prod BOOLEAN NOT NULL DEFAULT FALSE
 );
@@ -26,3 +29,9 @@ VALUES
     ('E_Solvents', 'E_Solvents_fi', 'E_Solvents_sv', 'E_Solvents_en', 'TRUE'),
     ('J_Waste', 'J_Waste_fi', 'J_Waste_sv', 'J_Waste_en', 'TRUE'),
     ('D_Fugitive', 'D_Fugitive_fi', 'D_Fugitive_sv', 'D_Fugitive_en', 'TRUE');
+
+
+UPDATE public.gnfr_meta SET 
+desc_fi='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 
+desc_sv='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 
+desc_en='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
