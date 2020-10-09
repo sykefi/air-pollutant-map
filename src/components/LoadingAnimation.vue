32f9f2b6e881a451250a15fa6c113b6bc2bdb7b0
Joose Helle <joose.helle@gmail.com>
2020-10-09 10:28:35 +0300

    Add loading animation to total pollution stats

--- src/components/LoadingAnimation.vue
+++ src/components/LoadingAnimation.vue
@@ -8,15 +8,25 @@
         xmlns:xlink="http://www.w3.org/1999/xlink"
         viewBox="0 0 80 80"
         class="loading-icon"
+        :style="size ? 'height:' + size + 'px;' : ''"
       >
         <path
-          fill="white"
+          :fill="color ? color : 'white'"
           d="M40,80C17.944,80,0,62.056,0,40C0,17.944,17.944,0,40,0c22.056,0,40,17.944,40,40c0,2.209-1.791,4-4,4s-4-1.791-4-4C72,22.355,57.645,8,40,8C22.355,8,8,22.355,8,40c0,17.645,14.355,32,32,32c2.209,0,4,1.791,4,4S42.209,80,40,80z"
         />
       </svg>
     </div>
   </div>
 </template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: {
+    color: String,
+    size: Number
+  }
+});
+</script>
 
 <style scoped>
 .loading-icon {
