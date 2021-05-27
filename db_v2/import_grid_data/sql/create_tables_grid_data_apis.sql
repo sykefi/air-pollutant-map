25ed8bdfb760282297764e97d3fd9c32f3a7ce73
Joose Helle <joose.helle@gmail.com>
2021-05-27 09:37:57 +0300

    refactor sql

--- db_v2/import_grid_data/sql/create_tables_grid_data_apis.sql
+++ db_v2/import_grid_data/sql/create_tables_grid_data_apis.sql
@@ -1,3 +1,20 @@
+
+
+-- grid_data_gnfr_dev
+
+DROP TABLE IF EXISTS grid_data_gnfr_dev;
+
+CREATE TABLE grid_data_gnfr_dev AS (
+    SELECT vuosi, grid_id, gnfr, s16,s15,s22,s13,s28,s29,s27,s43,s5,s18,s3,s12,s1,s7,s8,s14,s37,s25,s19,s17,s38,s40 FROM grid_data_import_temp
+);
+
+CREATE INDEX gnfr_dev_year_gnfr_idx ON grid_data_gnfr_dev (vuosi, gnfr);
+GRANT SELECT ON grid_data_gnfr_dev TO paastotkartalla;
+
+
+
+-- grid_data_gnfr_prod
+
 DROP TABLE IF EXISTS public.gnfr_dev_prod_map;
 
 CREATE TABLE public.gnfr_dev_prod_map (
@@ -35,6 +52,20 @@ CREATE TABLE public.grid_data_gnfr_prod AS (
 CREATE INDEX gnfr_prod_year_gnfr_idx ON grid_data_gnfr_prod (vuosi, gnfr);
 GRANT SELECT ON grid_data_gnfr_prod TO paastotkartalla;
 
-
 -- drop temporary tables
 DROP TABLE gnfr_dev_prod_map;
+
+
+
+-- grid_data_totals
+
+DROP TABLE IF EXISTS grid_data_totals;
+
+CREATE TABLE grid_data_totals AS (
+  SELECT vuosi, grid_id, sum(s16) as s16,sum(s15) as s15,sum(s22) as s22,sum(s13) as s13,sum(s28) as s28,sum(s29) as s29,sum(s27) as s27,sum(s43) as s43,sum(s5) as s5,sum(s18) as s18,sum(s3) as s3,sum(s12) as s12,sum(s1) as s1,sum(s7) as s7,sum(s8) as s8,sum(s14) as s14,sum(s37) as s37,sum(s25) as s25,sum(s19) as s19,sum(s17) as s17,sum(s38) as s38,sum(s40) as s40
+  FROM grid_data_import_temp
+  GROUP BY vuosi, grid_id
+);
+
+CREATE INDEX grid_data_totals_year_idx ON grid_data_totals (vuosi);
+GRANT SELECT ON grid_data_totals TO paastotkartalla;
